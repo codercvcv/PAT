8903a6481879c694d4e01e2ac2148a4dbbb16934
shuigen.zhan <814974481@qq.com>
2022-03-25 17:16:36 +0800

    A+B 和 C (15 分)

---
+++ Basic Level/B1011.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int N;
+  cin >> N;
+  long long num1, num2, num3;
+  for (int i = 1; i <= N; ++i)
+  {
+    cin >> num1 >> num2 >> num3;
+    (num1 + num2 > num3) ? printf("Case #%d: true\n", i) : printf("Case #%d: false\n", i);
+  }
+
+  return 0;
+}
