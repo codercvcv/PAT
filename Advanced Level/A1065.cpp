bdbb04934d627456ebbf7b0105c9cce3e102ccb1
shuigen.zhan <814974481@qq.com>
2022-03-28 15:43:14 +0800

    A+B and C (64bit) (20 分)

---
+++ Advanced Level/A1065.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n;
+  long long a, b, c;
+  cin >> n;
+  for (int i = 1; i <= n; ++i) {
+    scanf("%lld %lld %lld", &a, &b, &c);
+    long long result = a + b;
+    if (a > 0 && b > 0 && result < 0) {
+      printf("Case #%d: true\n", i);
+    } else if (a < 0 && b < 0 && result >= 0) {
+      printf("Case #%d: false\n", i);
+    } else {
+      result > c ? printf("Case #%d: true\n", i) : printf("Case #%d: false\n", i);
+    }
+  }
+
+  return 0;
+}
