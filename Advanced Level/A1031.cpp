706f7dcf1aec2cb2f6045acdb73b7b1f53df563c
shuigen.zhan <814974481@qq.com>
2022-04-02 14:27:36 +0800

    Hello World for U (20 分)

---
+++ Advanced Level/A1031.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  string str;
+  cin >> str;
+  int col = 0;
+  const int len = str.size();
+  int n1 = (len + 2) / 3, n3 = n1, n2 = len + 2 - n1 - n3;
+
+  for (int i = 0; i < n1 - 1; ++i) {
+      printf("%c", str[i]);
+      for (int j = 0; j < n2 - 1; ++j) printf(" ");
+      printf("%c\n", str[len - i - 1]);
+  }
+
+  for (int i = 0; i < n2; ++i) {
+    printf("%c", str[n1 + i - 1]);
+  }
+
+  return 0;
+}
