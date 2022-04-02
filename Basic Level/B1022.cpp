f3ab7ba92706e0c40fbd664a6d4968f3c3a974d7
shuigen.zhan <814974481@qq.com>
2022-04-02 15:47:04 +0800

    D进制的A+B (20 分)

---
+++ Basic Level/B1022.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int ans[50];
+  int A, B, D;
+  cin >> A >> B >> D;
+  int C = A + B;
+  int i = 0;
+  while (C) {
+    ans[i++] = C % D;
+    C /= D;
+  }
+
+  i == 0 ? printf("0"): printf("");
+  for (int j = i - 1; j >= 0; j--) {
+    printf("%d", ans[j]);
+  }
+
+  return 0;
+}
